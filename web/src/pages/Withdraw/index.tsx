934ecae58c03ac0570e0a29d63db8a0ac8d2fd50
httpsbruno <bruno-bbs80@hotmail.com>
2022-08-04 02:12:34 -0300

    adicionando withdraw

--- web/src/pages/Withdraw/index.tsx
+++ web/src/pages/Withdraw/index.tsx
@@ -3,6 +3,10 @@ import { InputAgency } from '../../components/InputAgency';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 import logoWithdraw from '../../images/logo-withdraw.svg';
+import { useState } from 'react';
+import { useUser } from '../../providers/UserProvider';
+import { Modal } from '../../components/Modal';
+import { DepositReceipt } from '../../components/DepositReceipt';
 
 /**
  * Archive: src/pages/Withdraw.tsx
@@ -11,34 +15,102 @@ import logoWithdraw from '../../images/logo-withdraw.svg';
  *
  * Date: 2022/07/20
  *
- * Author: Rey
+ * Author: Bruno
  */
 
 export const Withdraw = () => {
+  const [modal, setModal] = useState(false);
+  const { user } = useUser();
+  const [cpf, setCpf] = useState('');
+  const [valor, setValor] = useState('');
+  const [password, setPassword] = useState('');
+  const [erroServer, setErroServer] = useState<string>('');
+  const [receipt, setReceipt] = useState<boolean>(false);
+
+  async function handleWithdraw() {
+    fetch('http://127.0.0.1:8080/withdraws', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        agency: user?.agency,
+        agency_dig: user?.agency_dig,
+        number: user?.number,
+        number_dig: user?.number_dig,
+        cpf: Number(cpf),
+        amount: Number(valor), 
+        password: Number(password)
+      })
+    }).then(res => res.json())
+      .then(res => {
+        if (res.messages[0]) {
+          setErroServer(res.messages);
+          setModal(false);
+        } else {
+          setErroServer('');
+          if(user){
+            user.amount -= res.data[0].total_amount_with_fees;
+            setValor(res.data[0].total_amount_with_fees);
+          }
+          setReceipt(true);
+        }
+      });
+  }
+
   return (
-    <div className='flex flex-col justify-between h-full overflow-auto'>
+    <div className='flex flex-col justify-start items-center h-full overflow-auto'>
       <Menu />
-      <div className="static flex flex-col dark:border dark:border-[#424245] items-start rounded-lg p-4">
-        <div className="flex items-start mb-2">
-          <img src={logoWithdraw} alt="Deposit" className='p-2'/>
-          <p className="w-24 h-8 not-italic font-medium text-lg leading-1 flex items-center text-yellow-600 flex-none flex-grow-0">
-          Saque
-          </p>
-        </div>
-        <p className="ml-2 h-9 not-italic font-medium text-lg  text-header-dark dark:text-header-light">Dados para Saque</p>
-        <div className="flex items-start p-0 w-64 h-20 flex-none  flex-grow-0">
-          <InputAgency placeholder='Agência' status={true} />
-          <InputAgency placeholder='Conta' status={true} />
-        </div>
-        <Input placeholder='Valor' type='text'/>
-        <Input placeholder='Senha' type='password'/>
-        <Button
-          category="primary"
-          label="Sacar"
-          onClick={() => {console.log('Clicou');}}
+      {modal && (
+        <Modal
+          title="Saque"
+          setModal={setModal}
+          handleConfirmModal={handleWithdraw}
         />
-      </div>
-      <div className='h-2'></div>
+      )}
+      {
+        !receipt ?
+          ( 
+            <>
+              <div className="static flex flex-col dark:border dark:border-[#424245] items-start rounded-lg p-4">
+                <div className="flex items-start mb-2">
+                  <img src={logoWithdraw} alt="Deposit" className='p-2'/>
+                  <p className="w-24 h-8 not-italic font-medium text-lg leading-1 flex items-center text-yellow-600 flex-none flex-grow-0">
+                    Saque
+                  </p>
+                </div>
+                <p className="ml-2 h-9 not-italic font-medium text-lg  text-header-dark dark:text-header-light">Dados para Saque</p>
+                <div className="flex items-start p-0 w-64 h-20 flex-none  flex-grow-0">
+                  <InputAgency placeholder='Agência' status={true} value={`${user?.agency}-${user?.agency_dig}`} />
+                  <InputAgency placeholder='Conta' status={true} value={`${user?.number}-${user?.number_dig}`} />
+                </div>
+                <Input placeholder='CPF' type='number' value={cpf} setTextValue={setCpf}/>
+                <Input placeholder='Valor' type='number' value={valor} setTextValue={setValor}/>
+                <Input placeholder='Senha' type='password' value={password} setTextValue={setPassword}/>
+                <Button
+                  category="primary"
+                  label="Sacar"
+                  onClick={() => setModal(true)}
+                />
+                {erroServer && <p>{erroServer}</p>}
+              </div>
+              <div className='h-2'></div>
+            </>
+          )
+          : 
+          (
+            <div className='mt-9'>
+              <button onClick={() => {
+                setReceipt(false);
+                setCpf('');
+                setValor('');
+                setPassword('');
+              }}>Voltar</button>
+              <DepositReceipt type='Saque' date={new Date(Date.now()).toLocaleDateString()} value={Number(valor)} />
+            </div>
+          )
+      }
     </div>
   );
 };
